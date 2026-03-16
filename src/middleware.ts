b9ae812b349777125b7331ec49dd29699504de3c
Innova Space <emorales@colprovidencia.cl>
2026-03-16 10:32:31 -0300

    Update matcher to exclude additional file types

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,5 +38,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|map|txt|woff|woff2|ttf)$).*)',
+  ],
 }
